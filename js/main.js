2a7483c2e930c11078ca5d4b60769a652c9c7d90
nurbek717 <nurbektukhtamishev@gmail.com>
2021-12-14 22:03:01 +0500

    js-rederBookmark

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 var addMovies = movies.slice(0,100); 
 
 var elCardImg = document.querySelector("#card__img")
+var elBookmarkList = document.querySelector("#bookmark-ul")
 var elForm = document.querySelector("#form");
 var elRatingInput = document.querySelector("#inputReting");
 var elTitleInput = document.querySelector("#inputname")
 var elSelectInput = document.querySelector("#input-select-reting");
 var elTitle = document.querySelector("#title")
 
+var elCardMoviesTemplate = document.querySelector("#card-moves").content
+var elCarBuutonTemplate = document.querySelector("#card-button-template").content
 
 
-var normolizedMovieList =  addMovies.map(movieItem =>{
+var normolizedMovieList = addMovies.map((movieItem, index )=>{
 	return {
+		id: index + 1,
 		title: movieItem.Title.toString(),
 		categories: movieItem.Categories,
 		rating: movieItem.imdb_rating,
@@ -31,101 +35,79 @@ var renderCategors = function (moveList, renderSelect) {
 		splititIyem.forEach(categoryItem => {
 			
 			var esListSlect = renderCategorsArry.includes(categoryItem)
+			
 			if (!esListSlect) {
 				renderCategorsArry.push(categoryItem)
 			}
 			
 		})
 		renderCategorsArry.sort()
-	
+		
 	})  
 	
-	 var selectOption = document.createDocumentFragment()
-		renderCategorsArry.forEach(function (option) {
-			var elepntOption = document.createElement("option")
-			console.log(elepntOption);
-			elepntOption.setAttribute("class", " ")
-			elepntOption.textContent = option
-			elepntOption.value = option
-
-		  selectOption.appendChild(elepntOption)
-		})
-   
-  elSelectInput.appendChild(selectOption)
-
+	var selectOption = document.createDocumentFragment()
+	renderCategorsArry.forEach(function (option) {
+		var elepntOption = document.createElement("option")
+		
+		elepntOption.setAttribute("class", " ")
+		elepntOption.textContent = option
+		elepntOption.value = option
+		
+		selectOption.appendChild(elepntOption)
+	})
+	
+	elSelectInput.appendChild(selectOption)
+	
 }
 
-
-
-
-
 renderCategors(normolizedMovieList, elSelectInput )
-
+normolizedMovieList.sort()
 
 function renderMovies(felimArry, wrapper) {
-	
+	var resultRender = document.createDocumentFragment()
 	felimArry.forEach( movis  => {
-		var newDiv1 = document.createElement("div");
-		var newDiv2	 = document.createElement("div");
-		var newH2 = document.createElement("h4");
-		var newpYour = document.createElement("h5");
-		var newRet = document.createElement("h6");
-		var newCategors = document.createElement("h6");
-		var newIMG = document.createElement("img");
-		var newBtnLink = document.createElement("a");
-		var newBtn = document.createElement("button");
-		var newBtnBookmar = document.createElement("button");
+		var moviesCard = elCardMoviesTemplate.cloneNode(true)
 		
+		moviesCard.querySelector("#moves-img").src = movis.imageLink;
+		moviesCard.querySelector("#moves-title").textContent = movis.title;
+		moviesCard.querySelector("#moves-year").textContent = `Year: ${movis.year}`;
+		moviesCard.querySelector("#moves-reting").textContent = `Reating: ${movis.rating}`;
+		moviesCard.querySelector("#moves-categors").textContent = movis.categories;
+		moviesCard.querySelector("#moves-youtobe").href = movis.youtubeLink;
+		moviesCard.querySelector("#bookmark").dataset.idJonSalom = movis.id
 		
-		newDiv1.setAttribute("class" , "card me-2 mb-3");
-		newDiv1.setAttribute("style" , "width: 20rem;");
+		resultRender.appendChild(moviesCard)
 		
-		newIMG.setAttribute("src", movis.imageLink);
-		newIMG.setAttribute("class" , "card-img-bottom card");
-		newIMG.setAttribute("style" , "height: 220px;");
-		
-		newH2.setAttribute("class",  "mt-4 ms-4 text-danger fw-bold flex-grow-1" )
-		newH2.textContent = movis.title;
-		
-		newpYour.setAttribute("class",  "mt-4 ms-4 d-blog  fw-bold" )
-		newpYour.textContent = "Movie_year:   " + movis.year;
-		
-		newRet.setAttribute("class",  "mt-4 ms-4  fw-bold" )
-		newRet.textContent = "Movie_reting:   " + movis.rating;
-		
-		newCategors.setAttribute("class", "fw-bold ms-4 mt-3 text-primary")
-		newCategors.textContent = "Categors: " + movis.categories
-		
-		newDiv2.setAttribute("class", "pt-4 pb-5 ")
-		newBtnLink.setAttribute("class" , "btn btn-outline-primary p-1 ms-3 fs-6");
-		newBtnLink.setAttribute("href" , movis.youtubeLink);
-		newBtnLink.textContent = "Watch trailer";
-		newBtn.setAttribute("class" , "btn btn-outline-info btn-sm p-1 ms-2 fs-6");
-		newBtn.textContent = "More info";
-		newBtnBookmar.setAttribute("class" , "btn btn-outline-success btn-sm p-1 ms-2 fs-6");
-		newBtnBookmar.textContent = "Bookmarks";
-		
-		
-		wrapper.appendChild(newDiv1)
-		newDiv1.appendChild(newIMG);
-		newDiv1.appendChild( newH2);
-		newDiv1.appendChild(newpYour);
-		newDiv1.appendChild(newRet);
-		newDiv1.appendChild(newCategors);
-		
-		newDiv1.appendChild(newDiv2)
-		newDiv2.appendChild(newBtnLink);
-		newDiv2.appendChild(newBtn);
-		newDiv2.appendChild(newBtnBookmar);
-		
-		elCardImg.appendChild(newDiv1)
 	})
+	elCardImg.innerHTML = null
+	elCardImg.appendChild(resultRender)
 	
 	elTitle.textContent = `Sear result: ${felimArry.length}`;
 } 
 
 renderMovies(normolizedMovieList, elCardImg)
 
+
+function renderBookmark(bookmarkList) {
+	var elItem = document.createDocumentFragment();
+	
+	bookmarkList.forEach((item) => {
+		var newLi = elCarBuutonTemplate.cloneNode(true)
+		
+		
+		newLi.querySelector("#moves-img").src = item.imageLink;
+		newLi.querySelector("#moves-title").textContent = item.title;
+		newLi.querySelector("#moves-year").textContent = `Year: ${item.year}`;
+		newLi.querySelector("#moves-reting").textContent = `Reating: ${item.rating}`;
+		newLi.querySelector("#moves-categors").textContent = item.categories;
+		newLi.querySelector("#moves-youtobe").href = item.youtubeLink;
+		newLi.querySelector("#button-danger").dataset.idJon = item.id
+		elItem.appendChild(newLi)
+	})
+	
+	elBookmarkList.innerHTML = null
+	elBookmarkList.appendChild(elItem)
+}
 
 
 var findMovies = function (title, minRating, genre) {
@@ -141,8 +123,6 @@ var findMovies = function (title, minRating, genre) {
 
 elForm.addEventListener('submit', function(evt){
 	evt.preventDefault()
-	var resalt = []
-	
 	var selectInput = elTitleInput.value.trim()
 	
 	var searchKey = new RegExp(selectInput, "gi");
@@ -164,3 +144,52 @@ elForm.addEventListener('submit', function(evt){
 	
 })
 
+
+
+let storage = window.localStorage
+
+var getBookmark = storage.getItem("bookmark")
+var bookArry = JSON.parse(getBookmark) || []
+
+renderBookmark(bookArry);
+
+elCardImg.addEventListener("click", (evt) => {
+	var elementId = evt.target.dataset.idJonSalom
+	
+	if (elementId) {
+		var fountItem = normolizedMovieList.find(function (item) {
+			if (item.id == elementId) {
+				return item		
+			}
+		})
+		console.log(fountItem);
+
+		
+		///inclodes
+		var resaltInclode = bookArry.includes(fountItem)
+		if (!resaltInclode) {
+			bookArry.push(fountItem)
+		}
+		
+	}
+	storage.setItem('bookmark', JSON.stringify(bookArry))
+	renderBookmark(bookArry)
+});
+
+
+elBookmarkList.addEventListener("click", (evt) =>{
+	 var elButton = evt.target.dataset.idJon
+		
+		if (elButton) {
+			var fountItem = bookArry.find(function (item) {
+				if (item.id == elButton) {
+					return item		
+				}
+			})
+			bookArry.splice(fountItem,1)
+			renderBookmark(bookArry)
+			
+			console.log(elButton);
+			
+		}
+})
